9836f022d3d70199621e7f365cb18c78ef5e9238
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-06-14 00:54:20 +0530

    Update CharacterMovement.cs

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Rigidbody playerRigidbody;
+    private Animator animationController;
+    private Transform playerTransform;
+
+    private float runSpeed = 0.2f;
+    private float laneChangeSpeed = 5f;
+
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
+        animationController = GetComponent<Animator>();
+        playerTransform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float move = Input.GetAxis("Horizontal");
+
+        if (move == 0)
+        {// Always face forward when no horizontal input
+            playerTransform.forward = Vector3.forward;
+        }
+        else
+        {// look changing side
+            playerTransform.forward = new Vector3(move*runSpeed, 0, 1).normalized;
+        }
+
+        playerTransform.position += new Vector3(move * laneChangeSpeed, 0, 1) * runSpeed;
     }
 }
